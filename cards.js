766d9812ecd6ec914ba1518f2da4d510784da1d5
isaac bustos <isaac.youthboard@futureindesign.org>
2017-11-16 15:05:01 -0700

    re commented out all the code i don't know how to use, probably has the wrong values to the variables.

--- cards.js
+++ cards.js
@@ -7,25 +7,25 @@ $(document).ready( function() {
     $(this).attr('src', 'cardassets/' + source )
  })
 })
-//i don't kniw if this code works
- var sampleArr = ["card1.png", "card2.png", "card3.png", "card4.png", ]
- "card5.png", "card6.png", "card7.png", "card8.png", "card1.png",
- "card.png2", "card3.png", "card4.png", "card5.png",
- "card6.png", "card7.png", "card8.png" ]
- when page refreshes, all card go random
- var randArr = []
- function getRandNum() {
-   var rand = Math.floor(Math.random() * 16 )
-   var aarVar = sampleArr[rand]
-   if (aarVar === undefined)
-     getRandNum()
-     else
-       randArr.push(aarVar)
-       delete sampleArr[rand]
- }
+//i don't kniw if this code works//
+// var sampleArr = ["card1.png", "card2.png", "card3.png", "card4.png", ]
+// "card5.png", "card6.png", "card7.png", "card8.png", "card1.png",
+// "card.png2", "card3.png", "card4.png", "card5.png",
+// "card6.png", "card7.png", "card8.png" ]
+// when page refreshes, all card go random
+// var randArr = []
+ //function getRandNum() {
+//   var rand = Math.floor(Math.random() * 16 )
+//   var aarVar = sampleArr[rand]
+//   if (aarVar === undefined)
+//     getRandNum()
+//     else
+//       randArr.push(aarVar)
+//       delete sampleArr[rand]
+ //}
 
- while (randArr.length < 16) {
-   getRandNum()
- }
+// while (randArr.length < 16) {
+//   getRandNum()
+// }
 
- console.log(randArr)
+// console.log(randArr)
